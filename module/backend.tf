10f3145db717b196f9df62b008e2c7b25ae6d539
ayushshakya1997 <ayushshakya1997@gmail.com>
2022-06-27 22:04:02 +0530

    add file3

--- module/backend.tf
+++ module/backend.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "s3" {
+    bucket         = "bimal-bucket1"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "bimal-table"
    
   }
 }
